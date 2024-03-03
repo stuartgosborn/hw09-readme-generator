65de9b8a0007c095fda10a96ec512cf3a66fcc46
Stuart Osborn <stuartgosborn22@gmail.com>
2024-03-03 17:16:52 -0600

    wrote intial questions for inquirer

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require('inquirer');
 
+
+// Sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions (GitHub username, email, instructions for contact)
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is the name of your project?', 
+    'Please provide a brief description of your project.',
+    'Please provide any installation instructions.' ,
+    'Please provide instuctions and examples for use.',
+    'What type of license does your project have?',
+    'Who contributed to the project?',
+    'What test, if any, did you write for the applicaiton?'
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
